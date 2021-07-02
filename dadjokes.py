4ab4ca6301c8e2a06f7a2e2bc43ae081532ad3f9
jmldennis <jmldennis@gmail.com>
2021-07-02 10:26:57 -0500

    Removed Spaces in dadjokes.py

--- dadjokes.py
+++ dadjokes.py
@@ -9,10 +9,8 @@ class Dad_Jokes():
     def get_joke(self):
         return requests.get(self.baseurl, headers=self.headers).json().get("joke")
         
-
 def main():
     print(Dad_Jokes().get_joke())
-    
-    
+      
 if __name__ == '__main__':
     main()
